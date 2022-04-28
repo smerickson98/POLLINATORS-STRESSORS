d342fbc175b5f41b6ad1c441b56a8396aad56476
smerickson98 <smerickson98@gmail.com>
2022-04-28 14:08:16 -0400

    update

--- pesticide use.py
+++ pesticide use.py
@@ -5,21 +5,37 @@ Created on Wed Apr 27 21:34:13 2022
 
 @author: shannonerickson
 """
+import pandas as pd
+import numpy as np
 
-hp = open('HighEstimate_AgPestUsebyCropGroup92to19.txt', 'r')
+#pd.set_option('display.max_rows', None, 'display.max_columns', None)
+
+
+#low_pes = pd.read_csv('LowEstimate_AgPestUsebyCropGroup92to19.txt')
+
+#print(low_pes)
+                     
+hp = open('LowEstimate_AgPestUsebyCropGroup92to19.txt', 'r')
 #open txt file with pesticide data
 #hp refers to high estimate pesticide use data
 #'r' is the mode that will specify the way to open the file, r will read the file
 
-#with open('HighEstimate_AgPestUsebyCropGroup92to19.txt') as p:
+#with open('LowEstimate_AgPestUsebyCropGroup92to19.txt') as p:
     #contents = p.read()
     #print(contents) 
     
+#lines = hp.readlines()
+#for line in lines:
+    #print(line)
+
 with hp as p:
     for line in p:
         print(line)
         
-        
-   
-    
+data_in = np.loadtxt('LowEstimate_AgPestUsebyCropGroup92to19.txt', dtype = int)
+
+idx = np.where(np.logical_and(data_in[:,0]>2, data_in[:,0]<6))[0]
+
+np.savetxt('xyz_filtered.txt', data_in[idx,:], fmt = '%d')
+
 
